72e54abc2bad90c1bc2258c2541884ddbf0df687
KevinBarrera <kev97barrera@gmail.com>
2023-08-29 21:37:25 -0600

    initial setup

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={styles.root}>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
+export default App;
